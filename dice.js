02462f2dcd8f9061b50b2bd09be1a519e0137f79
Shouryaaagarwal <134755650+Shouryaaagarwal@users.noreply.github.com>
2024-01-11 11:58:35 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,18 +1,4 @@
-// var randoNumber1 = Math.floor(Math.random() * 6) + 1;
-// var diceImages = "dice" + randoNumber1 + ".png";
-// var randomImages = "images/" + diceImages;
-// document.querySelectorAll("img")[0].setAttribute("src", randomImages);
-// var randoNumber2 = Math.floor(Math.random() * 6) + 1;
-// var diceImages2 = "dice" + randoNumber2 + ".png";
-// var randomImages2 = "images/" + diceImages2;
-// document.querySelectorAll("img")[1].setAttribute("src", randomImages2);
-// if (randoNumber1 > randoNumber2) {
-//     document.querySelector("h1").innerHTML = "Player 1 wins ";
-// } else if (randoNumber1 < randoNumber2) {
-//     document.querySelector("h1").innerHTML = "Player 2 wins";
-// } else if (randoNumber1 === randoNumber2) {
-//     document.querySelector("h1").innerHTML = "Its a Draw ";
-// }
+
   
 
 var randomNumber1 = Math.floor(Math.random()*6)+1 ;
